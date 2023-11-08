0e19eacad23db427b5ebd7d3d762b844b3986bfd
Martin Milojkovic <martin.milo94@hotmail.com>
2023-11-08 21:33:08 +0100

    1

--- pia12iosv2mon/ContentView.swift
+++ pia12iosv2mon/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedTab = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-                        Text("Hurra version1")
+            HStack {
+                
+                Button(action: {
+                    selectedTab = 0
+                }, label: {
+                    Text("A")
+                        .foregroundColor(Color.white)
+                        .frame(maxWidth: .infinity,maxHeight:.infinity)
+                        .background(selectedTab == 0 ? Color.red : Color.gray)
+                })
+                
+               
+                    
+                Button(action: {
+                    selectedTab = 1
+                }, label: {
+                    Text("B")
+                        .foregroundColor(Color.white)
+                        .frame(maxWidth: .infinity,maxHeight:.infinity)
+                        .background(selectedTab == 1 ? Color.red : Color.gray)
+                })
+            }
+            .frame(height: 100.0)
+            .frame(maxWidth: .infinity)
+           
+            
+            
+            Spacer()
+            
+            if selectedTab == 0 {
+                Text("Nu är det A")
+                    .font(.largeTitle)
+            }
+            if selectedTab == 1 {
+                Text("B är vald")
+                    .font(.largeTitle)
+            }
+            
+           
+            
+            Spacer()
         }
-        .padding()
+        
+        
+    }
+    
+    func dosomecolor() -> Color {
+        return Color.cyan
     }
     
     func dofunstuff () {
