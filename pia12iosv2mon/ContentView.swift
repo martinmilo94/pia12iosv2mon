9be25349e39ba7267297b71d5395e299f0dcc597
Martin Milojkovic <martin.milo94@hotmail.com>
2023-11-08 15:30:10 +0100

    Initial Commit

---
+++ pia12iosv2mon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pia12iosv2mon
+//
+//  Created by Martin Milojkovic on 2023-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pia12iosv2mon/pia12iosv2monApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia12iosv2monApp.swift
+//  pia12iosv2mon
+//
+//  Created by Martin Milojkovic on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct pia12iosv2monApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
